2e5efde156910b5e7d4e9651709735bc8b7c6ab1
Tobias Sjösten <tobias@tobiassjosten.net>
2017-03-15 23:32:32 +0100

    Test layered includes

--- test/NormalizationTest.php
+++ test/NormalizationTest.php
@@ -191,4 +191,89 @@ class NormalizationTest extends \PHPUnit\Framework\TestCase
             ],
         ]));
     }
+
+    public function testNormalizeLayeredInclude()
+    {
+        $employeeId = uniqid();
+        $employeeName = uniqid();
+        $companyId = uniqid();
+        $companyName = uniqid();
+        $groupId = uniqid();
+        $groupName = uniqid();
+
+        $this->assertEquals([
+            'data' => [
+                'type' => 'employees',
+                'id' => $employeeId,
+                'attributes' => [
+                    'name' => $employeeName,
+                ],
+                'relationships' => [
+                    'company' => [
+                        'data' => ['type' => 'companies', 'id' => $companyId],
+                    ],
+                ],
+            ],
+            'included' => [
+                [
+                    'type' => 'companies',
+                    'id' => $companyId,
+                    'attributes' => [
+                        'name' => $companyName,
+                    ],
+                    'relationships' => [
+                        'group' => [
+                            'data' => ['type' => 'groups', 'id' => $groupId],
+                        ],
+                    ],
+                ],
+                [
+                    'type' => 'groups',
+                    'id' => $groupId,
+                    'attributes' => [
+                        'name' => $groupName,
+                    ],
+                ],
+            ],
+        ], jsonapi_normalize([
+            'data' => [
+                'type' => 'employees',
+                'id' => $employeeId,
+                'attributes' => [
+                    'name' => $employeeName,
+                ],
+                'relationships' => [
+                    'company' => [
+                        'data' => [
+                            'type' => 'companies',
+                            'id' => $companyId,
+                            'attributes' => [
+                                'name' => $companyName,
+                            ],
+                            'relationships' => [
+                                'group' => [
+                                    'data' => [
+                                        'type' => 'groups',
+                                        'id' => $groupId,
+                                        'attributes' => [
+                                            'name' => $groupName,
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'included' => [
+                            [
+                                'type' => 'groups',
+                                'id' => $groupId,
+                                'attributes' => [
+                                    'name' => $groupName,
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]));
+    }
 }
