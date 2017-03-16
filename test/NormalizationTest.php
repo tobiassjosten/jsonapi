46950f71f471236a8562bd76717d6d8485b87f76
Tobias Sjösten <tobias@tobiassjosten.net>
2017-03-16 12:58:18 +0100

    Normalization can be used recursively

--- test/NormalizationTest.php
+++ test/NormalizationTest.php
@@ -370,6 +370,82 @@ class NormalizationTest extends \PHPUnit\Framework\TestCase
         ]));
     }
 
+    public function testNormalizeDeepRecursed()
+    {
+        $employeeId = uniqid();
+        $employeeName = uniqid();
+        $companyId = uniqid();
+        $companyName = uniqid();
+        $groupId = uniqid();
+        $groupName = uniqid();
+
+        $this->assertEquals([
+            'data' => [
+                'type' => 'employees',
+                'id' => $employeeId,
+                'attributes' => [
+                    'name' => $employeeName,
+                ],
+                'relationships' => [
+                    'company' => [
+                        'data' => ['type' => 'companies', 'id' => $companyId],
+                    ],
+                ],
+            ],
+            'included' => [
+                [
+                    'type' => 'companies',
+                    'id' => $companyId,
+                    'attributes' => [
+                        'name' => $companyName,
+                    ],
+                    'relationships' => [
+                        'group' => [
+                            'data' => ['type' => 'groups', 'id' => $groupId],
+                        ],
+                    ],
+                ],
+                [
+                    'type' => 'groups',
+                    'id' => $groupId,
+                    'attributes' => [
+                        'name' => $groupName,
+                    ],
+                ],
+            ],
+        ], jsonapi_normalize([
+            'data' => [
+                'type' => 'employees',
+                'id' => $employeeId,
+                'attributes' => [
+                    'name' => $employeeName,
+                ],
+                'relationships' => [
+                    'company' => jsonapi_normalize([
+                        'data' => [
+                            'type' => 'companies',
+                            'id' => $companyId,
+                            'attributes' => [
+                                'name' => $companyName,
+                            ],
+                            'relationships' => [
+                                'group' => [
+                                    'data' => [
+                                        'type' => 'groups',
+                                        'id' => $groupId,
+                                        'attributes' => [
+                                            'name' => $groupName,
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ]),
+                ],
+            ],
+        ]));
+    }
+
     public function testNormalizeLayeredInclude()
     {
         $employeeId = uniqid();
