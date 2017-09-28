317943d397c6d95e2bd012a237cdc3a1d97c0664
Tobias Sjösten <tobias@tobiassjosten.net>
2017-09-28 15:49:52 +0200

    Make sure normalization is idempotent

--- test/NormalizationTest.php
+++ test/NormalizationTest.php
@@ -163,6 +163,53 @@ class NormalizationTest extends \PHPUnit\Framework\TestCase
         ]));
     }
 
+    public function testNormalizeTwice()
+    {
+        $employeeId = uniqid();
+        $employeeName = uniqid();
+        $companyId = uniqid();
+        $companyName = uniqid();
+
+        $this->assertEquals([
+            'data' => [
+                'type' => 'employees',
+                'id' => $employeeId,
+                'attributes' => [
+                    'name' => $employeeName,
+                ],
+                'relationships' => [
+                    'company' => [
+                        'data' => ['type' => 'companies', 'id' => $companyId],
+                    ],
+                ],
+            ],
+            'included' => [
+                [
+                    'type' => 'companies',
+                    'id' => $companyId,
+                    'attributes' => [
+                        'name' => $companyName,
+                    ],
+                ],
+            ],
+        ], jsonapi_normalize(jsonapi_normalize([
+            'type' => 'employees',
+            'id' => $employeeId,
+            'attributes' => [
+                'name' => $employeeName,
+            ],
+            'relationships' => [
+                'company' => [
+                    'type' => 'companies',
+                    'id' => $companyId,
+                    'attributes' => [
+                        'name' => $companyName,
+                    ],
+                ],
+            ],
+        ])));
+    }
+
     public function testNormalizeDuplicate()
     {
         $employeeId = uniqid();
